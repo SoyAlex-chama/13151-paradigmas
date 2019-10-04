a484be943e38188fff1500e759e248245e64099f
V.Alex <V.Alex.e.rivera@gmail.com>
2019-10-04 00:27:17 -0300

    Orden de los TDA y otros

--- lab1-worms.scm
+++ lab1-worms.scm
@@ -1,15 +1,8 @@
-; TDA
-; A player has:
-; * Position in X
-; * Position in y
-; * Life
-; * The turn in 1 or 0
+
+;TDA of Scenario
 ; A scene has:
 ; * Size of x
 ; * Size of y
-; A shoot has:
-; * Angle
-
 (define (retdaScenario)
   (display "N debe ser menor a X e Y.")
   (newline)
@@ -23,8 +16,21 @@
   (cons (cons sizeX sizeY) (cons numEnemies '())) (retdaScenario) )
 )
 
-(define (tdaPlayer posX posY life turn)
-  (cons (cons x y) (cons life (cons 4 '()))))
+;TDA of Player
+; A player has:
+; * Position in X
+; * Position in y
+; * Life
+; * The turn in 1 or 0
+; * The ultimate sense for their next turn
+
+(define (tdaPlayer posX posY life turn sense)
+  (cons (cons x y) (cons life (cons turn (cons sense '())))))
+
+; A shoot has:
+; * Angle
+; * Pressure?
 
 (define (tdaShoot angle pressure)
   (cons angle pressure))
+
