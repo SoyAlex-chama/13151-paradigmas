6c714176bf73e6a3f5dee467da90e4fa9896f8cf
V.Alex <V.Alex.e.rivera@gmail.com>
2019-10-05 14:29:22 -0300

    Una ultima :(

--- lab1-worms.rkt
+++ lab1-worms.rkt
@@ -4,15 +4,19 @@
 (require "randomScheme.rkt")
 
 (define (createScene N M E D seed)
+ (tdaScene N M E)
  (display "In construction")
   )
 
 (define (checkScene scene)
-  (if (and (positive? (selectorPosX scene))
+  (if (and
+       ;Validation of the scenario
+       (and (positive? (selectorPosX scene))
            (and (positive? (selectorPosY scene))
                 (and (< (selector scene 1) (selectorPosX scene)) (< (selector scene 1) (selectorPosY scene)))
                 )
            )
+           ()
       #t
       #f
       )
